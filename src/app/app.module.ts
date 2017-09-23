9520350dd793d55e523df72ce0f86b30a8fc3e9a
syrine mzoughi <syrine_mzoughi@yahoo.fr>
2017-09-23 16:32:47 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
+import { Routes, RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule }    from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import {  RoutingModule }  from './routing/routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FormulaireResumeComponent } from './formulaire-resume/formulaire-resume.component';
+import { UserService } from './formulaire-resume/user.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FormulaireResumeComponent
   ],
   imports: [
-    BrowserModule
-  ],
-  providers: [],
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    
+    RoutingModule,
+],
+
+  providers: [UserService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
